700d0875aec2e3fea5e5501ae36829af54e5556a
EyesOnly1987 <53872441+EyesOnly1987@users.noreply.github.com>
2020-01-31 08:05:46 -0700

    updated script, clean no error python script.

--- app/core/models.py
+++ app/core/models.py
@@ -8,8 +8,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         if not email:
-             raise ValueError('Users must have an email address')
-
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -24,6 +23,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
